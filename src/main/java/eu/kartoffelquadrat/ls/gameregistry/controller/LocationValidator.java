577f57cc43dbc48fb4fb4fed445ae77976551ecf
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2021-01-15 15:19:19 -0500

    Lifted gameservice location restriction to numerical IPs. Lobby-Service no also accepts lower-case docker-container identifiers as locators instead of classic IP.

--- src/main/java/eu/kartoffelquadrat/ls/gameregistry/controller/LocationValidator.java
+++ src/main/java/eu/kartoffelquadrat/ls/gameregistry/controller/LocationValidator.java
@@ -10,14 +10,15 @@ import java.util.regex.Pattern;
 public class LocationValidator {
 
     /**
-     * Validates whether a provided string is a valid IP address with port information.
+     * Validates whether a provided string is a valid IP address oder docker identifier (lower case string over
+     * alphabet [a-z]) plus (both cases) a port information.
      *
      * @param location the provided server location string
      * @return the validation result
      */
     public static boolean isValidGameServiceLocation(String location) {
         return Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|" +
-                "2[0-4][0-9]|25[0-5]):[0-9]+").matcher(location).find();
+                "2[0-4][0-9]|25[0-5])|[a-z]*:[0-9]+").matcher(location).find();
     }
 
     /**

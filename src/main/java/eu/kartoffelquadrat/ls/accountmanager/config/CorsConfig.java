3c28230aa5704f38cb14b2bd2a917767b28fde98
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2021-08-02 11:07:18 +0200

    Enabled CORS support for Oauth2 Tokens

---
+++ src/main/java/eu/kartoffelquadrat/ls/accountmanager/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package eu.kartoffelquadrat.tokenservice;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/**
+ * Configuration that injects a new http security filter with highest priority into springs request handler mechanism.
+ * This novel filter matches any CORS preflight request and returns an OK + CORS mandated header fields.
+ * This way the /oauth/token endpoints can be invoked from browsers pages of foreign origin.
+ * Note: This bean should not be renamed to "CorsConfiguration", as it sould then collide with a spring-internal bean.
+ */
+@Configuration
+public class CorsConfig {
+
+    // https://stackoverflow.com/a/42208796
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
+        config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
